4970b40af0bd43490d5c65bac1fcc1f9e6f7db35
Chad Banicki <cbanicki@homeaway.com>
2016-01-07 22:03:22 -0600

    able to finally get the x-axis to show the date property

--- plot2.R
+++ plot2.R
@@ -1,4 +1,8 @@
-plot1 <- function() {
+plot2 <- function() {
+  #Plot Global Active Power (Kilowatts) in a line graph
+  #with Weekday Names on the x-axis
+  #and the y-axis in increments of 2, starting from 0
+  
   
   library(dplyr)  
   
@@ -41,38 +45,51 @@ plot1 <- function() {
   # Change Date field format from String to Date
   Pwr$Date <- as.Date(Pwr$Date, format="%d/%m/%Y")
   
+
+  
  GAP <- 
    Pwr %>%
-   #Rounding the results up to the nearest 0.25
-   #mutate(GW = floor(Global_active_power/.50)*.50) %>%
-   mutate(Day = weekdays(Pwr$Date)) 
-   
+   #mutate(Day = weekdays(Pwr$Date)) %>%
+   mutate(DateTime = as.POSIXct(paste(Date, Time), format="%Y-%m-%d %H:%M:%S")) %>%
+   #mutate(Day = format(seq.Date(as.Date(Date), by = 'day', len = 1), "%a")) %>%
+   select(Date, DateTime, Global_active_power) %>%
+   group_by(Date)
+    
  
-  WeekDays <- unique(GAP$Day)
-  
-
-#GAP
-
-  # Bar Plot 
-    counts <- c(Pwr$Global_active_power)
+    counts <- c(GAP$Global_active_power)
     
-    gapRange <- range(0,counts)
+    day <- c(GAP$DateTime)
     
-    plot(counts, type="l", col="black", ylim=gapRange,
-         axes=FALSE, ann=TRUE)
+    dateShort <- c(GAP$Date)
     
-    axis(1, lab="Global Active Power (kilowatts)")
+    MaxPower <- max(counts)
+ 
     
-    
-    text(axTicks(1), 
-         labels=c(WeekDays),
-         xpd=T, cex=0.8)
+    # Get all the weekdays for the datetimes in the data
+#     weekday <- c(1:length(day))
+#     for (i in 1:length(day) ) {
+#       weekday[i] <- format(seq.Date(as.Date(day[i]), by = 'day', len = 1), "%a")
+#     }
+#     
+#     weekday <- unique(weekday)
+            
+          
+         
+         plot(day,counts,ylim=c(0,MaxPower),axes=FALSE,ylab= "Global Active Power (kilowatts)", type='l', col='black',xaxt = "n") 
+         #
+         points(x=GAP$Date, y=counts, col='black', type='l', lwd=2)
+         
+         axis.POSIXct(1, day, format="%a") #%m/%d") 
+        
+         #y-axis labels
+         axis(2, las=1, at=2*0:MaxPower)
+         
     
     # Create box for graph border
     box()
+
     
-    
-#     dev.copy(png,"plot2.png",width=8,height=6,units="in",res=100)
+#     dev.copy(png,"plot2.png",width=480,height=480,units="in",res=100)
 #     dev.off()
 # 
 #   
